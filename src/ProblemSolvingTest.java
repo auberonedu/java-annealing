11c53741d36ccbebff3185ee81677ec2539c08eb
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-04-01 15:22:18 -0700

    Added a JUnit test for empty set

--- src/ProblemSolvingTest.java
+++ src/ProblemSolvingTest.java
@@ -34,6 +34,15 @@ public class ProblemSolvingTest {
   //  - test a set where none start with A
   //  - test a set with only a single element
   //  - more you can think of!
+  @Test
+  void testAllStartWithA_emptySet() {
+    // arrange
+    Set<String> input = Set.of();
+    // act
+    boolean actual = ProblemSolving.hasEmptyString(input);
+    // assert
+    assertFalse(actual);
+  }
 
   @Test
   void testHasEmptyString_falseAllNonEmpty() {
