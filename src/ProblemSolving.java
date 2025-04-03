f13219520d34090038781b295c999514be859cc0
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-04-03 03:53:50 -0700

    Added maxLength in ProblemSolving.java

--- src/ProblemSolving.java
+++ src/ProblemSolving.java
@@ -46,8 +46,15 @@ public class ProblemSolving {
    * @return the maximum length of a word in the set
    */
   public static int maxLength(Set<String> words) {
-    return 0;
-  }
+    int max = 0;
+
+    for (String word : words) {
+        if (word.length() > max) {
+          max = word.length(); // updates the max if the word is longer than the previous max length
+        }
+    }
+    return max; // returns the length of the word
+}
 
  /**
    * Returns the length of the shortest word in the set.
