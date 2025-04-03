60f0b82b24ea6eb1ca90ae8cde31a1ae3418ca6a
TavSingh <tavparsadsingh2@gmail.com>
2025-04-02 22:29:13 -0700

    Completed first method

--- src/ProblemSolving.java
+++ src/ProblemSolving.java
@@ -12,7 +12,11 @@ public class ProblemSolving {
    * @return true if every word starts with A (case-insensitive), false otherwise.
    */
   public static boolean allStartWithA(Set<String> words) {
-    return false;
+    if (words.isEmpty());
+    for (String word : words) {
+      if (!word.startsWith("A") || !word.startsWith("a")) return false;
+    }
+    return true;
   }
 
   /**
