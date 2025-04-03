fbae18e47a89c46b3a087f78183e5b1671e1c46a
JSaiyan <183049245+JSaiyan@users.noreply.github.com>
2025-04-02 20:19:29 -0700

    completed test cases

--- src/ProblemSolvingTest.java
+++ src/ProblemSolvingTest.java
@@ -74,7 +74,7 @@ public class ProblemSolvingTest
   //  - more you can think of!
 
 
-  
+
   @Test
   void testHasEmptyString_falseAllNonEmpty() 
   {
@@ -97,6 +97,29 @@ public class ProblemSolvingTest
     assertTrue(actual);
   }
 
+  
+  @Test
+  void testHasEmptyString_falseBlankSpace() 
+  {
+    // arrange
+    Set<String> input = Set.of("armadillo", " ", "utopia", "yeah"); //no blank space is true in the logic
+    // act
+    boolean actual = ProblemSolving.hasEmptyString(input);
+    // assert
+    assertFalse(actual);
+  }
+
+  @Test
+  void testHasEmptyString_falseNoBlankSpace() 
+  {
+    // arrange
+    Set<String> input = Set.of("armadillo", "nope", "utopia", "yeah"); //no blank space is true in the logic
+    // act
+    boolean actual = ProblemSolving.hasEmptyString(input);
+    // assert
+    assertFalse(actual);
+  }
+
 
   // TODO:
   // Come up with more tests to thoroughly test hasEmptyString
@@ -114,12 +137,67 @@ public class ProblemSolvingTest
     assertEquals(9, actual);
   }
 
+  @Test
+  void testMaxLength_singleWord() 
+  {
+    // arrange
+    Set<String> input = Set.of("vicus");
+    // act
+    int actual = ProblemSolving.maxLength(input);
+    // assert
+    assertEquals(5, actual);
+  }
+
+  @Test
+  void testMaxLength_emptyWord() 
+  {
+    // arrange
+    Set<String> input = Set.of();
+    // act
+    int actual = ProblemSolving.maxLength(input);
+    // assert
+    assertEquals(0, actual);
+  }
+
 
   // TODO:
   // Come up with more tests to thoroughly test maxLength
   // Use your creativity here!
     
   
+
+  @Test
+  void testMinLength_emptyWord() 
+  {
+    // arrange
+    Set<String> input = Set.of();
+    // act
+    int actual = ProblemSolving.minLength(input);
+    // assert
+    assertEquals(Integer.MAX_VALUE, actual);
+  }
+
+  @Test
+  void testMinLength_multipleWords() 
+  {
+    // arrange
+    Set<String> input = Set.of("by", "a", "commodius", "vicus");
+    // act
+    int actual = ProblemSolving.minLength(input);
+    // assert
+    assertEquals(1, actual);
+  }
+
+  @Test
+  void testMinLength_singleWord() 
+  {
+    // arrange
+    Set<String> input = Set.of("vicus");
+    // act
+    int actual = ProblemSolving.minLength(input);
+    // assert
+    assertEquals(5, actual);
+  }
   // TODO:
   // Come up with ALL tests to thoroughly test minLength
   // Use your creativity here, and consider looking back at the maxLength
