222034e30156696611d6dd87d507e2efbfd79b55
DevRudy <55453823+SketchRudy@users.noreply.github.com>
2025-04-03 23:48:07 -0700

    Cleverly added a test case for empty strings

--- src/ProblemSolvingTest.java
+++ src/ProblemSolvingTest.java
@@ -90,6 +90,16 @@ public class ProblemSolvingTest {
   // Come up with more tests to thoroughly test hasEmptyString
   // Use your creativity here!
 
+  @Test
+  void testHasEmptyString_falseHasCharacter() {
+    // arrange
+    Set<String> input = Set.of(" ");
+    // act
+    boolean actual = ProblemSolving.hasEmptyString(input);
+    // assert
+    assertFalse(actual);
+  }
+
 
   @Test
   void testMaxLength_multipleWords() {
